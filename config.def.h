49d73223e3d9214ad8a53398cb5541f7cc5af5ba
furgelisherpa <furgeli3@gmail.com>
2022-08-27 00:56:56 +0545

    added fonts

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const char *fonts[] = {
                                   "JetBrains Mono:size=9:antialias=true:hinting=true",
                                   "SymbolsNerdFont:size=9:antialias=true:autohint=true",
+                                  "NotoColorEmoji:size=9:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
